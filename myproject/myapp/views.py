99740b60c4386b817548c92bc008ac85ac2b5ea9
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-08-09 09:16:45 +0530

    Sending data to template file

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
-    return render(request, 'index.html')
+    name = 'Mark'
+    return render(request, 'index.html', {'name':name})
+
+
+def counter(request):
+    # Safely get the 'text' parameter from the request, defaulting to an empty string if it's not provided
+    text = request.GET.get('text', '')
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
